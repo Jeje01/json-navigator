331def58d541cdae431294f5e8e465f43e89564a
jeje01 <jje141@gmail.com>
2022-03-27 22:41:00 +0900

    json 파일 업로드 기능 추가, 파일 바꿨을 떄 열린 계층 없어지도록 처리

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,16 +6,33 @@ import "./home.scss";
 
 const Home = () => {
   const [hierarchy, setHierarchy] = useState<object>()
+  const [jsonData, setJsonData] = useState<object>(data)
+  const [currentFileName, setCurrentFileName] = useState<string>()
+
+  const changeHandler = (event: any) => {
+    const fileReader = new FileReader();
+    fileReader.readAsText(event.target.files[0], "UTF-8");
+    setCurrentFileName(event.target.files[0].name)
+    fileReader.onload = (event) => {
+      if (event?.target?.result) {
+        setJsonData(JSON.parse(event.target.result as string))
+      }
+    }
+  }
 
   useEffect(() => {
-    setHierarchy(handleJSON(data))
-  }, [])
+    setHierarchy(handleJSON(jsonData))
+  }, [jsonData])
 
   return hierarchy ? (
-    <section className="home">
     <div className="home">
       <h1>JSON navigator</h1>
-      <List hierarchy={hierarchy} />
+      <section className="json-info">
+        <label htmlFor="upload_json">JSON 파일 업로드</label>
+        <input type="file" accept=".json" id="upload_json" onChange={changeHandler} />
+        <p>{`${currentFileName || "기본 데이터"} 보는 중`}</p>
+      </section>
+      <List key={currentFileName} hierarchy={hierarchy} />
     </div>
   ) : <>Loading...</>
 }
